7f31ea42b670a6793e8190b34bbd5e4d061e8c83
AmaniALnabhani <amanialnabhani7@gmail.com>
2023-12-25 20:59:16 +0400

    first page design done

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
-class StartScreen extends StatelessWidget{
+
+class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return  Center(child:Column(
-      children: [
-        Image.asset('assets/images/quiz-logo.png'),
-
-      ],
-    ),);
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300,
+          color: const Color.fromARGB(101, 255, 255, 255),),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter in fun way',
+            style: TextStyle(
+              color: Color.fromARGB(255, 0, 0, 0),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+            ),
+            label: const Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+          ),
+        ],
+      ),
+    );
   }
-
 }
